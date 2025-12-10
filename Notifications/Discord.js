6a53511382ff6bc9cec5ee9276373eb95616da6c
Romain Duarte <rduarte2@myges.fr>
2025-12-10 15:50:08 +0100

    Discord

---
+++ Notifications/Discord.js
@@ -0,0 +1,9 @@
+class Discord {
+    constructor (compte) {
+        this.compte = compte;
+    }
+}
+  send(message) {
+        console.log(`Sending message to ${this.address}: ${message}`);
+    }
+module.exports = discord;
