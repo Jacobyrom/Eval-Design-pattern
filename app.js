6a95df2348cc238be96c13c84e9712b489bd0949
Romain Duarte <rduarte2@myges.fr>
2025-12-10 15:19:59 +0100

    création du app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const CameraA = require('./capteurs/CapteurA/CameraA');
+const TemperatureSensorA = require('./capteurs/CapteurA/TemperatureSensorA');
+const MotionSensorA = require('./capteurs/MotionSensorA');
+const ThermalSensorB = require('./capteurs/ThermalSensorB');
+const ThermalSensorBEncaps = require('./capteurs/ThermalsensorBEncaps');
+
