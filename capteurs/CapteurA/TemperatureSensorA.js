126355cb6b50093873f09c58539b0e64ff1f35e9
Romain Duarte <rduarte2@myges.fr>
2025-12-10 15:26:57 +0100

    modifications TemperatureSensorA

--- capteurs/CapteurA/TemperatureSensorA.js
+++ capteurs/CapteurA/TemperatureSensorA.js
@@ -1,5 +1,12 @@
 TemperatureSensorA
 class TemperatureSensorA {
- constructor(location, threshold) {} // location = nom de la pièce, threshold = température de déclenchement
- detect() {} // Déclenche un message d’alerte (string)
+ constructor(location, threshold) {
+    this.location = location;
+    this.threshold = threshold;
+    this.type = "TemperatureSensorA";
+ }
+ detect() {
+    return `Alerte de ${this.type} dans la pièce ${this.location} avec une température de ${this.threshold}`;
+ }
 }
+module.exports = TemperatureSensorA;
